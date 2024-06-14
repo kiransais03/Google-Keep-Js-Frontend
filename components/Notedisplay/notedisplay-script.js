2554b80a5302c746d671b03c783584a0bbc30a53
kiransai <kiransais03@gmail.com>
2024-06-15 00:03:57 +0530

    savefunc

--- components/Notedisplay/notedisplay-script.js
+++ components/Notedisplay/notedisplay-script.js
@@ -17,12 +17,12 @@ async function getnotesanddisplay (notesdiv,usernotesarr) {
        noteuidiv.classList.add("noteui");
        noteuidiv.innerHTML = `<div >
         <div class='notetitlerow'>
-          <input class='note-input' style="font-weight:500;font-size:1.2rem,background-color:white" type="text" placeholder="" value="${notearrobj.title}" onchange="onchangedata()">
-           <button class='roundbtn' onclick="onchangedata()"><img src="../../svg/pin.svg" alt="pin"></button>
+          <input class='note-input' style="font-weight:500;font-size:1.2rem,background-color:white" type="text" placeholder="Enter Title" value="${notearrobj.title}">
+           <button class='roundbtn' onclick="onclickpin(this)" data-pinselected='${notearrobj.pinselected}'><img src="../../svg/pin.svg" alt="pin"></img></button>
           </div>
         <div style="display:flex;justify-content:flex-start;padding-left:10px;column-gap:5px">
            ${ notearrobj.labels.map((label,index)=>{
-             return (`<div>
+             return (`<div class="labelbtnsdiv">
               <button class="roundbtn" style="background-color:white;font-size:12px;border:1px solid black;padding:3px">${label} x</button>
              </div>`)
           })}
@@ -31,13 +31,13 @@ async function getnotesanddisplay (notesdiv,usernotesarr) {
        <div class='notebtnrow' style="${true?'display:flex':'display:none'}">
         <div style="display:flex;align-items:center">
           <img width="20px" style="background-color:white" src="../../svg/backgroundcoloricon.svg" alt='bgcoloricon'/> ᐅᐅ
-          <input type="color" style="border-radius:5px;cursor:pointer"}} value="#000000" onchange="onchangedata()">
-         <button class='roundbtn' onclick="onchangedata()"><img width="20px" src="../../svg/archives.svg" alt="archivesicon"></button>
-          <button class='roundbtn' onclick="onchangedata()"><img width="20px" src="../../svg/deleteforevericon.svg" alt="deleteforevericon"></button>
+          <input type="color" style="border-radius:5px;cursor:pointer"}} value="#000000">
+         <button class='roundbtn archivebtn' data-isarchived='${notearrobj.archived}' onclick="onchangedata()"><img width="20px" src="../../svg/archives.svg" alt="archivesicon"></button>
+          <button class='roundbtn trashbtn' data-istrashed='${notearrobj.trashed}' onclick="onchangedata()"><img width="20px" src="../../svg/deleteforevericon.svg" alt="deleteforevericon"></button>
          <button class='roundbtn' onclick="onchangedata()"><img width="20px" src="../../svg/restoreicon.svg" alt="restoreicon"></button>
         </div>
         <div>
-        <button class='roundbtn' styles="font-weight:600;background-color:white" onclick="onchangedata()">Save</button>
+        <button class='roundbtn' styles="font-weight:600;background-color:white" onclick="editandsavenotesfunc(this)">Save</button>
         </div>
       </div>`
       
@@ -45,3 +45,52 @@ async function getnotesanddisplay (notesdiv,usernotesarr) {
      })   
 }
 
+
+//Notes component button functions
+
+function onclickpin (elem) {
+   let pinboolean = JSON.parse(elem.getAttribute('data-pinselected'));
+   if(pinboolean) {
+     elem.setAttribute('data-pinselected',"false");
+     elem.innerHTML = '<img src="../../svg/pinselected.svg" alt="pin"></img>'
+   }
+   else {
+    elem.setAttribute('data-pinselected',"true");
+    elem.innerHTML = '<img src="../../svg/pin.svg" alt="pin"></img>'
+   }
+}
+
+async function editandsavenotesfunc (elem) {
+  try {
+  // 3 parents for save
+  let currnotediv = elem.parentElement.parentElement.parentElement;
+  console.log(currnotediv,"uidiv");
+  let inputtitle = currnotediv.querySelector('.notetitlerow input').value;
+  let textareavalue = currnotediv.querySelector('textarea').value;
+  let labelbtnsnodelist = currnotediv.querySelectorAll('.labelbtnsdiv button');
+  let labelbtnselemarr = Array.from(labelbtnsnodelist);
+  let labelnamesarr = labelbtnselemarr.map((elem,index)=>{
+    let innertext= elem.innerText;
+    let labelname = innertext.slice(0,innertext.length-2);
+    return labelname;
+  })
+  let inputcolor = currnotediv.querySelector('input[type="color"]').value;
+  let ispinselected = JSON.parse(currnotediv.querySelector('.notetitlerow button').getAttribute('data-pinselected'));
+  let isarchived = JSON.parse(currnotediv.querySelector('.archivebtn').getAttribute('data-isarchived'));
+  let istrashed = JSON.parse(currnotediv.querySelector('.trashbtn').getAttribute('data-istrashed'))
+  console.log(inputtitle,textareavalue,labelnamesarr,inputcolor,ispinselected,isarchived,istrashed);
+
+  let editandsaveresponse =  await fetch("http://localhost:8080/notes/editlabel",{method:"PATCH","headers":
+    {'Content-Type':"application/json",
+      'Token-Googlekeep':"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImtpcmFuc2FpczAzIiwibmFtZSI6IktpcmFuIFNhaSIsImVtYWlsIjoia2lyYW5zYWlzMDNAZ21haWwuY29tIiwidXNlcklkIjoiNjY2NzI2MTVhZWFmNzgwNDgyZDAzNjE2IiwiaWF0IjoxNzE4MDM2MDI2fQ.SnZ5u3T9PzwowrpyW2AxaqzJ2Nmd2FkTC2dCQRx9NLs"
+    },body:JSON.stringify({
+        "email":"kiransais03@gmail.com",
+        "oldlabel":oldlabelname,
+        "editedlabel":editedlabelname
+    })});
+let labelsarrdata = await labelsresponse.json();
+
+  } catch (error) {
+    console.log("Failed to edit notes",error)
+  }
+}
--- pages/Editlabelspage/editlabelspage-script.js
+++ pages/Editlabelspage/editlabelspage-script.js
@@ -1,5 +1,6 @@
 
 let navbarhomepagediv = document.getElementsByClassName('navbarhomepagediv')[0];
+let labelsdisplaydiv = document.getElementsByClassName('showlabels')[0];
 
 function clickchangedata () {
     console.log("Onchange called")
@@ -34,15 +35,6 @@ async function shownavbarcomponent () {
     
 }
 
-async function loadscriptfilesonebyone () {
-    await shownavbarcomponent();
-    await getlabelslist();
-   }
-   
-loadscriptfilesonebyone();
-
-let labelsdisplaydiv = document.getElementsByClassName('labelsdisplay')[0];
-
 async function getlabelslist () {
     let labelsresponse =  await fetch("http://localhost:8080/notes/getlabelslist",{method:"GET","headers":
         {'Content-Type':"application/json",
@@ -51,13 +43,134 @@ async function getlabelslist () {
     let labelsarrdata = await labelsresponse.json();
     console.log(labelsarrdata.notesarr[0].labelslist,"response data")
     let labelslistarr = labelsarrdata.notesarr[0].labelslist;
+    labelsdisplaydiv.innerHTML=''
     labelslistarr.map((currelem,index)=>{
        let labeldiv = document.createElement('div');
        labeldiv.classList.add('labelrow');
-       labeldiv.innerHTML = `<button class='roundbtn' onclick="clickchangedata()"><img width="20px" src="../../svg/trash.svg" alt="trash"></button>
-            <input class='note-input'  type="text" placeholder="Enter Label Name" value=${currelem} onchange="clickchangedata()">
-            <button class='roundbtn' onclick="clickchangedata()" style="font-weight:400"><img src="../../svg/editicon.svg" alt="edit"></button>`
+       console.log("name",currelem)
+       labeldiv.innerHTML = `<button class='roundbtn' data-labelrownum=${index} onclick="deletelabelfunc(this)"><img width="20px" src="../../svg/trash.svg" alt="trash"></button>
+            <input class='note-input' disabled  type="text" placeholder="Enter Lable Name" value='${currelem}'>
+            <button class='roundbtn editbtn' data-labelrownum=${index} onclick="editlabelclicked(this)" style="font-weight:400"><img src="../../svg/editicon.svg" alt="edit"></button>`
             labelsdisplaydiv.append(labeldiv);
             clickchangedata()
     })
 }  
+
+async function loadscriptfilesonebyone () {
+    await shownavbarcomponent();
+    await getlabelslist();
+   }
+   
+
+loadscriptfilesonebyone();
+
+//Buttons action functions
+
+function clearinputfunc () {
+  let newlabelinputelem = document.getElementById('newlabelinput');
+  newlabelinputelem.value = ""
+}
+
+async function createlabelfunc () {
+    try {
+        let newlabelinputelem = document.getElementById('newlabelinput');
+        let labelname = newlabelinputelem.value;
+        let labelsresponse =  await fetch("http://localhost:8080/notes/addlabel",{method:"POST","headers":
+            {'Content-Type':"application/json",
+              'Token-Googlekeep':"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImtpcmFuc2FpczAzIiwibmFtZSI6IktpcmFuIFNhaSIsImVtYWlsIjoia2lyYW5zYWlzMDNAZ21haWwuY29tIiwidXNlcklkIjoiNjY2NzI2MTVhZWFmNzgwNDgyZDAzNjE2IiwiaWF0IjoxNzE4MDM2MDI2fQ.SnZ5u3T9PzwowrpyW2AxaqzJ2Nmd2FkTC2dCQRx9NLs"
+            },body:JSON.stringify({
+                "email":"kiransais03@gmail.com",
+                "labelname":labelname
+            })});
+        let labelsarrdata = await labelsresponse.json();
+        console.log("Label created successfully",labelsarrdata);
+        await getlabelslist()
+        
+    }
+    catch(err) {
+       console.log("Failed to create new label")
+    }
+    
+
+}
+
+
+async function deletelabelfunc (elem) {
+  try {
+    console.log(elem)
+    let labelrownum = parseInt(elem.getAttribute('data-labelrownum'));
+    let deletinglabelname = document.querySelectorAll('div.labelrow input')[labelrownum].value
+    // let deletinglabelname = document.getElementsByClassName('labelrow')[labelrownum+1].children[1].value
+    let labelsresponse =  await fetch("http://localhost:8080/notes/deletelabel",{method:"DELETE","headers":
+        {'Content-Type':"application/json",
+          'Token-Googlekeep':"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImtpcmFuc2FpczAzIiwibmFtZSI6IktpcmFuIFNhaSIsImVtYWlsIjoia2lyYW5zYWlzMDNAZ21haWwuY29tIiwidXNlcklkIjoiNjY2NzI2MTVhZWFmNzgwNDgyZDAzNjE2IiwiaWF0IjoxNzE4MDM2MDI2fQ.SnZ5u3T9PzwowrpyW2AxaqzJ2Nmd2FkTC2dCQRx9NLs"
+        },body:JSON.stringify({
+            "email":"kiransais03@gmail.com",
+            "labelname":deletinglabelname
+        })});
+    let labelsarrdata = await labelsresponse.json();
+    console.log("Label deleted successfully",labelsarrdata)
+    await getlabelslist()
+
+  } catch (error) {
+    console.log("Failed to delete the label",error)
+  }
+}
+
+//storing old unedited label name
+let oldlabelname ="";
+let editclicked = false;
+
+
+async function editlabelclicked (elem) {
+    if(editclicked) {
+        await editlabelfuncclick2(elem);
+        editclicked=false;
+    } 
+    else {
+        await editlabelfuncclick1(elem);
+        editclicked=true;
+    }
+}
+
+
+async function editlabelfuncclick1 (elem) {
+    try {
+        let labelrownum = parseInt(elem.getAttribute('data-labelrownum'));
+        let inputform = document.querySelectorAll('div.labelrow input')[labelrownum];
+        oldlabelname = inputform.value;
+        inputform.disabled = false;
+        inputform.focus()
+        elem.innerHTML = `<img src="../../svg/tickicon.svg" alt="tickicon">`;
+        
+  
+    } catch (error) {
+      console.log("Failed to edit the label",error)
+    }
+  }
+
+
+  async function editlabelfuncclick2 (elem) {
+    try {
+      let labelrownum = parseInt(elem.getAttribute('data-labelrownum'));
+         let editedlabelname = document.querySelectorAll('div.labelrow input')[labelrownum].value;
+      let labelsresponse =  await fetch("http://localhost:8080/notes/editlabel",{method:"PATCH","headers":
+          {'Content-Type':"application/json",
+            'Token-Googlekeep':"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImtpcmFuc2FpczAzIiwibmFtZSI6IktpcmFuIFNhaSIsImVtYWlsIjoia2lyYW5zYWlzMDNAZ21haWwuY29tIiwidXNlcklkIjoiNjY2NzI2MTVhZWFmNzgwNDgyZDAzNjE2IiwiaWF0IjoxNzE4MDM2MDI2fQ.SnZ5u3T9PzwowrpyW2AxaqzJ2Nmd2FkTC2dCQRx9NLs"
+          },body:JSON.stringify({
+              "email":"kiransais03@gmail.com",
+              "oldlabel":oldlabelname,
+              "editedlabel":editedlabelname
+          })});
+      let labelsarrdata = await labelsresponse.json();
+      let inputform = document.querySelectorAll('div.labelrow input')[labelrownum];
+      inputform.disabled = true;
+      elem.innerHTML = `<img src="../../svg/editicon.svg" alt="edit">`
+
+      console.log("Label edited successfully",labelsarrdata)
+      await getlabelslist();
+  
+    } catch (error) {
+      console.log("Failed to edit the label",error)
+    }
+  }
